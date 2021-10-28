fb06e6f86683576e58cc7a9b6f173b757075ca49
Marlene <marlene.piekny@le-campus-numerique.fr>
2021-10-28 09:52:33 +0200

    init with spring initializer file

---
+++ src/main/java/com/marlene/apiview/ApiviewApplication.java
@@ -0,0 +1,13 @@
+package com.marlene.apiview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marlene/apiview/ApiviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marlene.apiview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
